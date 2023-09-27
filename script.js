2eeb91aec5b1a879c6388b2542dde933a8d61160
RicardoDesenvolvimentus <144399660+RicardoDesenvolvimentus@users.noreply.github.com>
2023-09-26 21:04:36 -0300

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-var navLinks = document.getElementById("navLinks");
-
-function showMenu() {
-  navLinks.style.visibility = "visible";
-}
-
-function hideMenu() {
-  navLinks.style.visibility = "hidden";
-}
